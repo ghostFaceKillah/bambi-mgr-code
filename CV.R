93c4fc98d2da6db7e3a01eaa9a403d8509b53934
ghostFaceKillah <garmulewicz@gmail.com>
2015-07-09 10:40:27 +0200

    zmiana pozycji lammbdy, h, lifting kodu

--- CV.R
+++ CV.R
@@ -1,67 +1,70 @@
 
-
-CrossValid<-function(x,LG,z,model){
+#for tps set model =  0 
+CrossValid<-function(x,LG,z,model)
+{
   
-  iterNum<-20
-  diagnostic<-list()
-  y<-coordinates(LG)
-  CVSmoothed<-list() 
+    iterNum<-100
+    diagnostic<-list()
+    y<-coordinates(LG)
+    CVSmoothed<-list() 
+    CVSmoothed<-replicate(5, matrix(0,127,1), simplify=F)
   
-  if(model==0)
-  {
-    diagnostic<-replicate(5, matrix(0,100,3), simplify=F)
-  }
-  else
-  {
-    diagnostic<-replicate(5, matrix(0,700,3), simplify=F)
-  }
-  
-  CVSmoothed<-replicate(5, matrix(0,127,1), simplify=F)
-  
-  for (i in 1:length(x))
-   {
-    lambda<-0
-    for (j in 1:iterNum)
-    {
-      lambda<-lambda+2
-      h<-6
-      if (model==0)
-      { 
-          tps<-Tps(y, x[[i]]$RATIO, lambda=lambda, lon.lat=TRUE)
-          fit<-fitted(tps)
-      }
-      else
-      {
-          for (k in 1:7)
-          {
-              h<-h+1
-              print(h)
-              fit<-smooth_whittaker(LG,h,lambda) 
-          }
-      }
-      error<-sum((fit-z[[i]]$RATIO)^2)
-      diagnostic[[i]][j,]<-c(lambda,error,h)
-    }
-    minlambda<-diagnostic[[i]][which.min(diagnostic[[i]][,2]),1]
-    print(minlambda)
     if(model==0)
-    { 
-      tps<-Tps(y, x[[i]]$RATIO, lambda=minlambda, lon.lat=TRUE)
-      fit<-fitted(tps)
+    {
+        diagnostic<-replicate(5, matrix(0,100,3), simplify=F)
     }
+    
     else
     {
-      minh<-diagnostic[[i]][which.min(diagnostic[[i]][,2]),3]
-      print(minh)
-      fit<-smooth_whittaker(LG,minh,minlambda) 
-    }    
-    
-      CVSmoothed[[i]]<-fit 
-    
-  }
+        diagnostic<-replicate(5, matrix(0,700,3), simplify=F)
+    }
   
-  return(c(CVSmoothed,diagnostic))
+    for (i in 1:length(x))
+    {
+        lambda<--0
+        
+        for (j in 1:iterNum)
+        {
+            h<-7
+            if (model==0)
+            { 
+                tps<-Tps(y, x[[i]]$RATIO, lambda=lambda, lon.lat=TRUE)
+                fit<-fitted(tps)
+                error<-sum((fit-z[[i]]$RATIO)^2)
+                diagnostic[[i]][j,]<-c(lambda,error,h)
+            }
+            
+            else
+            {
+                for (k in 1:7)
+                {
+                    fit<-smooth_whittaker(LG,h,lambda)
+                    error<-sum((fit-z[[i]]$RATIO)^2)
+                    diagnostic[[i]][j*7+k-7,]<-c(lambda,error,h)
+                    h<-h+1
+                }
+            }
+        }
+        
+        minlambda<-diagnostic[[i]][which.min(diagnostic[[i]][,2]),1]
+        
+        if(model==0)
+        { 
+            tps<-Tps(y, x[[i]]$RATIO, lambda=minlambda, lon.lat=TRUE)
+            fit<-fitted(tps)
+        }
+        
+        else
+        {
+            minh<-diagnostic[[i]][which.min(diagnostic[[i]][,2]),3]
+            fit<-smooth_whittaker(LG,minh,minlambda) 
+        }    
+    
+        CVSmoothed[[i]]<-fit 
+        lambda<-lambda+0.001
+    }
   
+    return(c(CVSmoothed,diagnostic))
 }
 
 CV<-CrossValid(fourfold.list,LGroup,onefold.list,1)
